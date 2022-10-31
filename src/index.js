a4889a88e9c57b37dc939312b2b122a9b52965b3
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-10-31 10:34:16 -0300

    agregamos conexion a base de datos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+//llamar a la conexion de la base de datos
+import "./databas";
 //crear una instancia de express
 const app = express();
 //configurar un puerto
@@ -7,6 +12,20 @@ app.listen( app.get("port"), ()=>{
     console.log("estoy en el puerto "+ app.get("port") );
 })
 //middlewares: son funciones q se ejecutan antes de llegr a las rutas
+app.use (cors());//permite peticiones remotas
+
+//permite recibir y usar objetos en formato json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+//inforacion extra
+app.use(morgan("dev"))
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname,"../public")))
+
 
 //rutas
+app.get("/", (req, res)=>{
+    res.send("hola desde el backend en la peticion get")
+
+})
 
