85e2efc8d4b69658632efbdbc2bb268b65903b02
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-10-31 21:26:16 -0300

    agrgamos la peticion d productos a la bd

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -1,14 +1,36 @@
-export const listarProductos = (req, res)=>{
-    res.send("hola desde el backend en la peticion get")
+import Producto from "../models/producto";
 
+export const listarProductos =async (req, res) => {
+  try {
+    //Buscar productos
+const productos = await Producto.find()
+
+    //Devolver al frontend el arreglo de productos
+    res.status(200).json(productos);
+  } catch (error) {
+  console.log(error)
+  //enviar respuesta al frontend
+  res.status(404).json({
+    mensaje: "Ocurrio un error"
+  })
+  }
 };
 
-export const crearProducto = (req, res)=>{
-
-    console.log(req.body)
+export const crearProducto = async (req, res) => {
+  try {
+    console.log(req.body);
     //validar el objeto recibido
-
-    //guardar en la bas de datos
-    res.send("aqui se tendria que crear un producto")
-
+    // creo el objeto con mongoose
+    const productoNuevo = new Producto(req.body);
+    //guardar en la base de datos
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "EL PRODUCTO FUE CREADO CORRECTAMENTE",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al intentar agregar un nuevo producto",
+    });
+  }
 };
